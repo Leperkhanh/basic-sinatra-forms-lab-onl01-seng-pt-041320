3ad3825e6c1c45441dc6915dade321d9aebaafea
Jordan Nguyen <jordan@DESKTOP-GP7UG4A.localdomain>
2020-06-27 12:37:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do 
+        erb :newteam
+    end
 
+    post '/team' do 
+        puts params
+        erb :team
+    end
+
+    get '/team' do 
+        erb :team
+    end
 
 end
